8388f29d02f0c2d2f0de7192e07565fd45d7ae93
Dmotti <dario.motti@gmail.com>
2014-02-13 18:26:36 -0500

    Finished dnacalc.py

--- dnacalc.py
+++ dnacalc.py
@@ -20,4 +20,21 @@ NumberC = DNASeq.count('C')
 NumberG = DNASeq.count('G')
 NumberT = DNASeq.count('T')
 # Calculate percentage and output to 1 decimal
+print "A: %.1f" % (100 * NumberA / SeqLength)
+print "C: %.1f" % (100 * NumberC / SeqLength)
+print "G: %.1f" % (100 * NumberG / SeqLength)
+print "T: %.1f" % (100 * NumberT / SeqLength)
 
+# Calculating primer melting points with different formulas by length
+
+TotalStrong = NumberG + NumberC
+TotalWeak = NumberA + NumberT
+
+if SeqLength >= 14:
+	#formula for sequences > 14 nucleotides long
+	MeltTempLong = 64.9 + 41 * (TotalStrong - 16.4) / SeqLength
+	print "Tm Long (>14): %.1f C" % (MeltTempLong)
+else:
+	#formula for sequences less than 14 nucleotides long
+	MeltTemp = (4 * TotalStrong) + (2 * TotalWeak)
+	print "Tm Short: %.1f C" % (MeltTemp)
