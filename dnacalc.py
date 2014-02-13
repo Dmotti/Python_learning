055a2acfd5c6917f0193ece79ae3db7ec8b79d62
Dmotti <dario.motti@gmail.com>
2014-02-13 12:59:29 -0500

    Adding length calculation to dnacalc.py

--- dnacalc.py
+++ dnacalc.py
@@ -9,4 +9,15 @@ DNASeq = DNASeq.replace(" ","") # remove spaces
 
 print 'Sequence:', DNASeq
 
+# below are nested functions: first find the length, then make it float
+SeqLength = float(len(DNASeq))
+
+# SeqLength = (len(DNASeq))
+print "Sequence Length: ", SeqLength
+
+NumberA = DNASeq.count('A')
+NumberC = DNASeq.count('C')
+NumberG = DNASeq.count('G')
+NumberT = DNASeq.count('T')
+# Calculate percentage and output to 1 decimal
 
