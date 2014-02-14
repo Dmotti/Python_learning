95acd28aacd4dc7caa887a9dfd23a966af3abc0c
Dmotti <dario.motti@gmail.com>
2014-02-14 18:28:01 -0500

    Dictionary for Aminoacids Mol Weight added to proteincalc.py

--- proteincalc.py
+++ proteincalc.py
@@ -1,4 +1,31 @@
 #! /usr/bin/env python
+
+AminoDict={
+'A':8,
+'R':1,
+'N':1,
+'D':1,
+'C':1,
+'Q':1,
+'E':1,
+'G':7,
+'H':1,
+'I':1,
+'L':1,
+'K':1,
+'M':1,
+'F':1,
+'P':1,
+'S':1,
+'T':1,
+'W':2,
+'Y':1,
+'V':1,
+'X':0,
+'-':0,
+'*':0,
+}
+
 ProteinSeq = "FDILSATFTYGNR"
 for AminoAcid in ProteinSeq:
 	print AminoAcid
